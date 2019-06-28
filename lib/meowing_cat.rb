7acfbe70bacc28b479194dfb876faaf059b5959b
magical-flow-3186 <rregalado17@gmail.com>
2019-06-28 17:33:30 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,6 +1,6 @@
 ## code your solution here. 
 class Cat 
   attr_accessor :name
-  attr_reader :meow ["meow!"]
+  attr_reader :meow="meow!" #is the reason I cant write the meow method as a attr_writer macro because it is an instance method?
   
 end
